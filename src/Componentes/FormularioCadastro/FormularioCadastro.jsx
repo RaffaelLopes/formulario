a9a4cb6a649efd643643ccf9effb5f88410d05fa
Rafael <91689189+RaffaelLopes@users.noreply.github.com>
2023-08-11 19:13:58 -0300

    Add files via upload

---
+++ src/Componentes/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import { TextField, Button, Switch, FormControlLabel } from '@material-ui/core';
+
+
+function FormularioCadastro() {
+    const [nome, setNome] = useState ("eek");
+    
+    return (
+        <form 
+        onSubmit={(event)=>  {
+            event.preventDefault();
+            console.log(nome);
+        }}>
+
+        <TextField
+        value={nome}
+        onChange={(event)=>  {
+           setNome(event.target.value);
+            if (nome.length >= 3) {
+                setNome(nome.substr(0, 3));
+            }
+        }}
+           
+            id="nome"
+            label="Nome"
+            variant="outlined"
+            margin="normal"
+            fullWidth />
+
+            <TextField
+            id="sobrenome"
+            label="Sobrenome"
+            variant="outlined"
+            margin="normal"
+            fullWidth />
+
+            <TextField
+            id="CPF"
+            label="CPF"
+            variant="outlined"
+            margin="normal"
+            fullWidth />
+
+            <FormControlLabel
+            label="Promoções"
+            control={<Switch
+            name="promocoes" defaultChecked
+            color='primary' />} />
+
+            <FormControlLabel
+            label="Novidades"
+            control={<Switch name="promocoes" defaultChecked
+            color='primary' />} />
+
+            <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
+
+        </form>)
+
+}
+
+export default FormularioCadastro;
